97d642c8e327c78deb33012ad4d3c2c7d741f5df
RahulVerma989 <RahulVerma989@users.noreply.github.com>
2024-07-01 20:22:54 +0530

    added cost in ProductDetails class. Updated products_api.yaml and models.py code.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, StrictInt, validator
+from pydantic import BaseModel, StrictInt, validator, StrictFloat, Field
+from typing import Optional
 from enum import Enum
 
 
@@ -25,12 +26,14 @@ class ProductDetails(BaseModel):
         name (str): The name of the product.
         type (ProductType): The type of the product, restricted to the ProductType enum.
         inventory (StrictInt): The quantity of the product in stock, must be an integer.
+        cost Optional(StrictFloat): The cost of the product in USD, must be an float. Default is 0.00 and value >= 0.00 and value <= 999.99
 
     The inventory field is validated to ensure it is an integer between 1 and 9999, inclusive.
     """
     name: str
     type: ProductType
     inventory: StrictInt  # Ensure only integers are accepted
+    cost: Optional[StrictFloat] = Field(default=0.00, le=999.99, ge=0.00)
 
     @validator('inventory')
     def check_inventory(cls, v):
@@ -50,6 +53,24 @@ class ProductDetails(BaseModel):
             raise ValueError('Inventory must be an integer')
         if v < 1 or v > 9999:
             raise ValueError('Value should be greater than 1 and less than 9999')
+        return v
+    
+    @validator('cost', pre=True, always=True)
+    def check_cost(cls, v):
+        """
+        Validate that the cost is an float within the specified range.
+
+        Args:
+            v (float): The cost value to validate.
+
+        Returns:
+            float: The validated inventory value.
+
+        Raises:
+            ValueError: If the inventory is None.
+        """      
+        if v is None:
+            raise ValueError('Cost must not be null')
         return v
 
 class Product(ProductDetails):
